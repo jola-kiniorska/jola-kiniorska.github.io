8766a696eb0a77f1b40feb62b2e79251fc3456a8
Jola Klodowska <jola.klodowska@wp.pl>
2024-08-06 09:00:03 +0200

    add desktop styles

--- src/sections/Navbar/Navbar.tsx
+++ src/sections/Navbar/Navbar.tsx
@@ -4,42 +4,78 @@ import icon_github from "../../images/icon_github.png";
 import icon_linkedin from "../../images/icon_linkedin.png";
 import icon_behance from "../../images/icon_behance.png";
 import SocialMedia from "../../components/SocialMedia/SocialMedia.tsx";
+import { isMobile } from "react-device-detect";
 
 const Navbar = () => {
+  const scroll = (section) => {
+    document
+      .querySelector(`.main_content-${section}`)
+      ?.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
 
-const scroll = (section) => {
-  document.querySelector(`.main_content-${section}`)?.scrollIntoView({behavior: "smooth", block: "center"});
-}
-
-  return (
-    <div className="navbar">
-      <div className="navbar_wrapper">
-        <div className="logo_link" onClick={() => scroll('home')}>
-          <img src={logo} alt="logo" />
+  const renderContent = () => {
+    if (isMobile) {
+      return (
+        <div className="navbar">
+          <div className="navbar_wrapper">
+            <div className="wrapper_nav-text">
+              <p className="nav-text" onClick={() => scroll("home")}>
+                Home
+              </p>
+              <p className="nav-text" onClick={() => scroll("about")}>
+                About
+              </p>
+              <p className="nav-text" onClick={() => scroll("projects")}>
+                Projects
+              </p>
+              <p className="nav-text" onClick={() => scroll("contact")}>
+                Contact
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="wrapper_nav-text">
-          <p className="nav-text" onClick={() => scroll('home')}>Home</p>
-          <p className="nav-text" onClick={() => scroll('about')}>About</p>
-          <p className="nav-text" onClick={() => scroll('projects')}>Projects</p>
-          <p className="nav-text" onClick={() => scroll('contact')}>Contact</p>
-        </div>
-        <div className="wrapper_social-media">
-          <SocialMedia
-            icon={icon_github}
-            link={"https://github.com/jola-klodowska"}
-          />
-          <SocialMedia
-            icon={icon_linkedin}
-            link={"https://www.linkedin.com/in/jolanta-klodowska/"}
-          />
-          <SocialMedia
-            icon={icon_behance}
-            link={"https://www.behance.net/jolaklodowska"}
-          />
+      );
+    }
+    return (
+      <div className="navbar">
+        <div className="navbar_wrapper">
+          <div className="logo_link" onClick={() => scroll("home")}>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="wrapper_nav-text">
+            <p className="nav-text" onClick={() => scroll("home")}>
+              Home
+            </p>
+            <p className="nav-text" onClick={() => scroll("about")}>
+              About
+            </p>
+            <p className="nav-text" onClick={() => scroll("projects")}>
+              Projects
+            </p>
+            <p className="nav-text" onClick={() => scroll("contact")}>
+              Contact
+            </p>
+          </div>
+          <div className="wrapper_social-media">
+            <SocialMedia
+              icon={icon_github}
+              link={"https://github.com/jola-klodowska"}
+            />
+            <SocialMedia
+              icon={icon_linkedin}
+              link={"https://www.linkedin.com/in/jolanta-klodowska/"}
+            />
+            <SocialMedia
+              icon={icon_behance}
+              link={"https://www.behance.net/jolaklodowska"}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  return renderContent();
 };
 
 export default Navbar;
